6416bdff31f0239e7d5a488264ea616057973437
Tin <55455483+TinSisko-gameDev@users.noreply.github.com>
2024-06-23 11:19:58 +0200

    Added Product model for the product list endpoint.

---
+++ KingZadatak/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace KingZadatak.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Thumbnail { get; set; }
+    }
+}
